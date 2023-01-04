e0bb6ba166390b6263cd9e8cd639604baf3cd4e4
agustin rosano <agustinrosano7@gmail.com>
2023-01-04 02:54:46 +0100

    falta mediaquery

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 const img = new Image()
-img.src = './back-flapy.png'
+img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
 
 
 //general settings 
@@ -17,46 +17,147 @@ const gravity = .5,
 let index = 0,
     bestScore = 0,
     flight,
-    flyheigt,
+    flyHeight,
     currentScore,
     pipe,
     pipes
+
+// pipe settings 
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
+
+const setup = () => {
+    currentScore = 0 
+    flight = jump
+
+    //set initial flyheight
+
+    flyHeight = (canvas.height / 2) - (size[1] / 2)
+
+    //setup first 3 pipes
+    pipes = Array(3).fill().map((a,i) => [
+        canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()
+    ])
+}
 
 const render = () => {
     //movimiento de pajaro
     index++
 
-    //background first part 
-    ctx.drawImage (
-        img,
-        0,
-        0,
-        canvas.width,
-        canvas.height,
-        -((index * (speed / 2)) % canvas.width ) + canvas.width,
-        0,
-        canvas.width,
-        canvas.height
-    )
+   // background first part 
+   ctx.drawImage(
+    img, 
+    0, 
+    0, 
+    canvas.width,
+    canvas.height,
+    -((index * (speed / 2)) % canvas.width) + canvas.width, 
+    0,
+    canvas.width, 
+    canvas.height);
+   // background second part
+   ctx.drawImage(
+    img, 
+    0, 
+    0, 
+    canvas.width, 
+    canvas.height, 
+    -(index * (speed / 2)) % canvas.width, 
+    0, 
+    canvas.width, 
+    canvas.height);
+   
 
-    //background second part 
+    // pipe display
+  if (gamePlaying){
+    pipes.map(pipe => {
+      // pipe moving
+      pipe[0] -= speed;
 
-    ctx.drawImage (
-        img,
-        0,
-        0,
-        canvas.width,
-        canvas.height,
-        -((index * (speed / 2)) % canvas.width ) + canvas.width,
-        0,
-        canvas.width,
-        canvas.height
-    )
+      // top pipe
+      ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+      // bottom pipe
+      ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
 
-//6:48   https://www.youtube.com/watch?v=bF-pTsdun40
+      // give 1 point & create new pipe
+      if(pipe[0] <= -pipeWidth){
+        currentScore++;
+        // check if it's the best score
+        bestScore = Math.max(bestScore, currentScore);
+        
+        // remove & create new pipe
+        pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]];
+        console.log(pipes);
+      }
+    
+      // if hit the pipe, end
+      if ([
+        pipe[0] <= cTenth + size[0], 
+        pipe[0] + pipeWidth >= cTenth, 
+        pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+      ].every(elem => elem)) {
+        gamePlaying = false;
+        setup();
+      }
+    })
+  }
 
-window.requestAnimationFrame(render)
+    
+
+   // draw bird
+  if (gamePlaying) {
+    ctx.drawImage(
+    img,
+    432, 
+    Math.floor((index % 9) / 3) * size[1], 
+    ...size, 
+    cTenth, 
+    flyHeight, 
+    ...size);
+
+    flight += gravity;
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+
+  } else {
+    ctx.drawImage(
+        img, 
+        432, 
+        Math.floor((index % 9) / 3) * size[1], 
+        ...size, 
+        ((canvas.width / 2) - size[0] / 2), 
+        flyHeight,
+        ...size
+    );
+
+    flyHeight = (canvas.height / 2) - (size[1] / 2);
+
+      // text accueil
+    ctx.fillText(`Best score : ${bestScore}`, 85, 245);
+    ctx.fillText('Click to play', 90, 535);
+    ctx.fillText('by: agustin rosano',14,650);
+    ctx.font = "bold 30px courier";
+    
+    
+  
+    
+  }
+
+  document.getElementById('bestScore').innerHTML = `Best : ${bestScore}`;
+
+  document.getElementById('currentScore').innerHTML = `Current : ${currentScore}`;
+    
+
+  window.requestAnimationFrame(render)
 
 }    
 
+setup()
 img.onload = render;
+
+
+//16:51 https://www.youtube.com/watch?v=bF-pTsdun40
+// https://codepen.io/ju-az/pen/eYJQwLx/
+//start game 
+document.addEventListener('click',() => gamePlaying = true)
+window.onclick = () => flight = jump
